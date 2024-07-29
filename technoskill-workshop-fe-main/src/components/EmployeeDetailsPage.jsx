ae1850e1f5429b61788d96daa2d1022094a7e4c1
Naufal Hadi Rasikhin <naufal.hdr153@gmail.com>
2024-07-29 21:12:17 +0700

    employee details

--- technoskill-workshop-fe-main/src/components/EmployeeDetailsPage.jsx
+++ technoskill-workshop-fe-main/src/components/EmployeeDetailsPage.jsx
@@ -6,11 +6,22 @@ import { EmployeeData } from "./HomePage";
 
 export default function EmployeeDetailsPage(){
   let employee_details = EmployeeData();
-  console.log(employee_details);
   return (
     <div className="bg-[#CED1DA] h-screen w-screen flex">
       <DashboardElement />
-      
+
+      <div className="bg-[#2B2E63] w-[622px] h-[675px] m-auto rounded-2xl flex flex-col text-white">
+        <img src={employeeIcon} className="w-[240px] mx-auto mt-24" />
+
+        <p className="text-[30px] mx-auto mt-20">My Info</p>
+
+        <div className="mx-auto mt-10">
+          <p className="text-[25px]">Name</p>
+          <div className="mx-auto rounded-2x1 text-white text-[25px]">
+            {name}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
