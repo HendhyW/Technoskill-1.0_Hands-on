a232dd0358f0778f3da385c21fced1a1259a3d49
Maxwell <maxwellzgt@gmail.com>
2024-07-29 21:10:29 +0700

    finished initial EmployeeDetailsPage

--- technoskill-workshop-fe-main/src/components/EmployeeDetailsPage.jsx
+++ technoskill-workshop-fe-main/src/components/EmployeeDetailsPage.jsx
@@ -4,12 +4,45 @@ import axios from 'axios';
 
 import { EmployeeData } from "./HomePage";
 
+import employeeIcon from "../assets/employee.svg";
+
 export default function EmployeeDetailsPage(){
+  
   let employee_details = EmployeeData();
   console.log(employee_details);
+
   return (
     <div className="bg-[#CED1DA] h-screen w-screen flex">
+      <div className="bg-[#CED1DA] h-screen w-screen flex">
       <DashboardElement />
+
+      <div className="bg-[#2B2E63] w-[622px] h-[675px] m-auto rounded-2xl flex flex-col text-white">
+        <img src={employeeIcon} className="w-[240px] mx-auto mt-24" />
+
+        <p className="text-[30px] mx-auto mt-15">Employee Info</p>
+
+        <div className="mx-auto mt-10">
+          <p className="text-[25px]">
+            <span>  Name: </span>
+            <span className="mx-auto rounded-2x1 text-white text-[25px]">
+              {employee_details['name']}
+            </span>
+          </p>
+          <p className="text-[25px]">
+            <span>  Division: </span>
+            <span className="mx-auto rounded-2x1 text-white text-[25px]">
+              {employee_details['division']}
+            </span>
+          </p>
+          <p className="text-[25px]">
+            <span>  Salary: </span>
+            <span className="mx-auto rounded-2x1 text-white text-[25px]">
+              {employee_details['salary']}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
       
     </div>
   )
